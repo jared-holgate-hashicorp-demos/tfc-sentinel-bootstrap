72394d12bfb918d26b8d21364b26433585c41568
richard-russell <richard.russell@hashicorp.com>
2022-02-11 17:33:39 +0000

    Alphabetisen variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,17 @@
+variable "github_token" {
+  type        = string
+  description = "github oauth token for creating repos and attaching policy set"
+}
+
+variable "organization" {
+  type        = string
+  description = "TFC/TFE organization used for the demo"
+}
+
 variable "prefix" {
   type        = string
   default     = "tfc-sentinel-demo"
   description = "String prefix for the workspace and policy-sets"
-}
-
-variable "tfguides_fork" {
-  type        = string
-  default     = "richard-russell/terraform-guides"
-  description = "Github path of the fork of hashicorp/terraform-guides used for the policies"
 }
 
 variable "self_service_template" {
@@ -16,20 +20,9 @@ variable "self_service_template" {
   default     = "richard-russell/tfc-sentinel-template-repo-aws"
 }
 
-# to-do: create trusted workspace
-# variable "trusted_template" {
-#   type        = string
-#   description = "Github repo to use as a template for the trusted repo in the demo"
-#   default     = "richard-russell/tfc-sentinel-template-repo-aws"
-# }
-
-variable "organization" {
+variable "tfguides_fork" {
   type        = string
-  description = "TFC/TFE organization used for the demo"
-}
-
-variable "github_token" {
-  type        = string
-  description = "github oauth token for creating repos and attaching policy set"
+  default     = "richard-russell/terraform-guides"
+  description = "Github path of the fork of hashicorp/terraform-guides used for the policies"
 }
 
