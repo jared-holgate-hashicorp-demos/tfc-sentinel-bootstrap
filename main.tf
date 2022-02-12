7e78aa3c1d00253e148a2159d2c64125b1c15265
richard-russell <richard.russell@hashicorp.com>
2022-02-12 18:26:01 +0000

    Initial refactor into submodule

--- main.tf
+++ main.tf
@@ -12,29 +12,6 @@ resource "random_pet" "prefix" {
   prefix = var.prefix
 }
 
-locals {
-  # split github path into owner and repository (sentinel policy forked repo)
-  sentinel_repo_tmp_list   = split("/", var.sentinel_repo)
-  sentinel_repo_owner      = local.sentinel_repo_tmp_list[0]
-  sentinel_repo_repository = local.sentinel_repo_tmp_list[1]
-
-  # split github path into owner and repository (self-service repo)
-  self_service_tmp_list            = split("/", var.self_service_template)
-  self_service_template_owner      = local.self_service_tmp_list[0]
-  self_service_template_repository = local.self_service_tmp_list[1]
-
-  # to-do: create trusted workspace
-  # # split github path into owner and repository (trusted repo)
-  # trusted_tmp_list            = split("/", var.trusted_template)
-  # trusted_template_owner      = local.trusted_tmp_list[0]
-  # trusted_template_repository = local.trusted_tmp_list[1]
-}
-
-# Existing repo forked from hashicorp/terraform-sentinel-policies
-data "github_repository" "sentinel_repo" {
-  full_name = var.sentinel_repo
-}
-
 # Create OAUTH client to use for VCS-backed repos and attaching policy-sets
 resource "tfe_oauth_client" "demo" {
   organization     = var.organization
@@ -42,64 +19,6 @@ resource "tfe_oauth_client" "demo" {
   http_url         = "https://github.com"
   oauth_token      = var.github_token
   service_provider = "github"
-}
-
-# Create self-service workspace and associated policies 
-
-# Create branch for self-service policy-set in the terraform-sentinel-policies fork
-resource "github_branch" "self-service-branch" {
-  repository    = local.sentinel_repo_repository
-  branch        = random_pet.prefix.id
-  source_branch = data.github_repository.sentinel_repo.default_branch
-}
-
-# Create copy of self-service template repo to use for a VCS backed workspace
-resource "github_repository" "self-service" {
-  name        = "${random_pet.prefix.id}-self-service"
-  description = "Self-service repo for ${random_pet.prefix.id} demo"
-
-  visibility = "public"
-
-  template {
-    owner      = local.self_service_template_owner
-    repository = local.self_service_template_repository
-  }
-}
-
-# Create a TFC self-service workspace
-resource "tfe_workspace" "selfservice" {
-  name         = "${random_pet.prefix.id}-self-service"
-  organization = var.organization
-  tag_names    = ["demo", "selfservice"]
-  auto_apply   = true
-  vcs_repo {
-    identifier     = github_repository.self-service.full_name
-    oauth_token_id = tfe_oauth_client.demo.oauth_token_id
-  }
-
-}
-
-# Apply policy sets
-resource "tfe_policy_set" "cloud-agnostic" {
-  name          = "${random_pet.prefix.id}-self-service-cloud-agnostic"
-  description   = "terraform-sentinel-policies cloud-agnostic policy set"
-  organization  = var.organization
-  policies_path = "cloud-agnostic"
-  workspace_ids = [tfe_workspace.selfservice.id]
-
-  vcs_repo {
-    identifier         = data.github_repository.sentinel_repo.full_name
-    branch             = random_pet.prefix.id
-    ingress_submodules = false
-    oauth_token_id     = tfe_oauth_client.demo.oauth_token_id
-  }
-}
-
-# Add policy-set parameter - the list of orgs whose PMRs we are allowed to use
-resource "tfe_policy_set_parameter" "orgs" {
-  key           = "organizations"
-  value         = "[ \"${var.organization}\" ]"
-  policy_set_id = tfe_policy_set.cloud-agnostic.id
 }
 
 # Add modules to PMR
@@ -112,4 +31,13 @@ resource "tfe_registry_module" "two-tier-registry-module" {
   }
 }
 
-# to-do: create trusted workspace and associated policies 
+# Create self-service workspace and associated policies 
+module "self-service-workspace" {
+  source             = "./modules/workspace"
+  organization       = var.organization
+  workspace_name     = "${random_pet.prefix.id}-self-service"
+  sentinel_repo      = var.sentinel_repo
+  workspace_template = var.self_service_template
+  tfe_oauth_token_id = tfe_oauth_client.demo.oauth_token_id
+}
+
---
+++ modules/workspace/main.tf
@@ -0,0 +1,94 @@
+variable "organization" {
+  type        = string
+  description = "The organization under which to create the workspace"
+}
+
+variable "sentinel_repo" {
+  type        = string
+  description = "The github Sentinel policy repo"
+}
+
+variable "tfe_oauth_token_id" {
+    type = string
+    description = "Github oauth token id for TFC"
+}
+
+variable "workspace_name" {
+  type        = string
+  description = "The name of the workspace"
+}
+
+variable "workspace_template" {
+  type        = string
+  description = "The github template to use for the workspace repo"
+}
+
+data "github_repository" "sentinel_repo" {
+  full_name = var.sentinel_repo
+}
+
+locals {
+  # split github path into owner and repository (sentinel policy repo)
+  sentinel_repo_tmp_list   = split("/", var.sentinel_repo)
+  sentinel_repo_owner      = local.sentinel_repo_tmp_list[0]
+  sentinel_repo_repository = local.sentinel_repo_tmp_list[1]
+
+  # split github path into owner and repository (workspace template repo)
+  workspace_tmp_list            = split("/", var.workspace_template)
+  workspace_template_owner      = local.workspace_tmp_list[0]
+  workspace_template_repository = local.workspace_tmp_list[1]
+}
+
+# Create branch for this workspace in the terraform-sentinel-policies fork
+resource "github_branch" "policies_branch" {
+  repository    = local.sentinel_repo_repository
+  branch        = var.workspace_name
+  source_branch = data.github_repository.sentinel_repo.default_branch
+}
+
+# Create copy of workspace template repo to use for a VCS backed workspace
+resource "github_repository" "workspace" {
+  name        = var.workspace_name
+  description = "Repo for ${var.workspace_name} workspace"
+  visibility  = "public"
+
+  template {
+    owner      = local.workspace_template_owner
+    repository = local.workspace_template_repository
+  }
+}
+
+# Create a TFC workspace
+resource "tfe_workspace" "workspace" {
+  name         = var.workspace_name
+  organization = var.organization
+  tag_names    = ["automated"]
+  auto_apply   = true
+  vcs_repo {
+    identifier     = github_repository.workspace.full_name
+    oauth_token_id = var.tfe_oauth_token_id
+  }
+}
+
+# Apply policy sets
+resource "tfe_policy_set" "cloud-agnostic" {
+  name          = "${var.workspace_name}-cloud-agnostic"
+  description   = "terraform-sentinel-policies cloud-agnostic policy set"
+  organization  = var.organization
+  policies_path = "cloud-agnostic"
+  workspace_ids = [tfe_workspace.workspace.id]
+
+  vcs_repo {
+    identifier         = data.github_repository.sentinel_repo.full_name
+    branch             = var.workspace_name
+    ingress_submodules = false
+    oauth_token_id     = var.tfe_oauth_token_id
+  }
+}
+
+# Add policy-set parameter - the list of orgs whose PMRs we are allowed to use
+resource "tfe_policy_set_parameter" "orgs" {
+  key           = "organizations"
+  value         = "[ \"${var.organization}\" ]"
+  policy_set_id = tfe_policy_set.cloud-agnostic.id
+}
