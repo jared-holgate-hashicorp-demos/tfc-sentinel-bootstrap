91a113866d0285dae3618c73eba6d8dcf43dceba
jaredfholgate <jared.holgate@hashicorp.com>
2022-06-15 10:01:51 +0100

    Update versions

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,12 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "4.20.0"
     }
     tfe = {
       source  = "hashicorp/tfe"
-      version = "0.28.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0"
     }
   }
 }
